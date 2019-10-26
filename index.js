ba40cbc7cdd39ba081acf3e50138a44df4926446
Chris Zuber <shgysk8zer0@gmail.com>
2019-10-26 16:05:47 -0700

    Add multiple layout options
    - default (header + nav = 100vh, right sidebar)
    - wide (no sidebar)
    - left-isdebar
    - right-sidebar
    - Set using `:root[data-layout]`
    - Add multi-columns options using `[data-columns]`
    - Multiple custom properties affect layout config

---
+++ index.js
@@ -0,0 +1,14 @@
+import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
+import 'https://cdn.kernvalley.us/js/std-js/shims.js';
+import 'https://cdn.kernvalley.us/components/share-button.js';
+import 'https://cdn.kernvalley.us/components/current-year.js';
+import 'https://cdn.kernvalley.us/components/bacon-ipsum.js';
+import 'https://cdn.kernvalley.us/components/toast-message.js';
+import {$, ready} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+
+document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
+
+ready().then(async () => {
+	$(':root').toggleClass('no-js', 'js');
+	$('select[name="layout"]').change(event => document.documentElement.dataset.layout = event.target.value);
+});
