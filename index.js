6381bc8970e5f3db75b52b5e6e2207fdf97d7f44
Chris Zuber <shgysk8zer0@gmail.com>
2020-02-04 13:30:52 -0800

    Allow adjusting article paragraphs for testing
    Useful for checking for different sizes of content

--- index.js
+++ index.js
@@ -5,26 +5,32 @@ import 'https://cdn.kernvalley.us/components/current-year.js';
 import 'https://cdn.kernvalley.us/components/bacon-ipsum.js';
 import 'https://cdn.kernvalley.us/components/toast-message.js';
 import 'https://cdn.kernvalley.us/components/gravatar-img.js';
-import {$, ready} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import { $, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 
 document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
 
 ready().then(async () => {
 	$(':root').toggleClass('no-js', 'js');
 
-	$('select[name="layout"]').change(event => {
-		if (event.target.value === '') {
+	$('#layout-picker').input(({ target }) => {
+		if (target.value === '') {
 			delete document.documentElement.dataset.layout;
 		} else {
-			document.documentElement.dataset.layout = event.target.value;
+			document.documentElement.dataset.layout = target.value;
 		}
-	}, {passive: true});
+	}, { passive: true });
 
-	$('select[name="theme"]').change(event => {
-		if (event.target.value === '') {
+	$('#paras').input(({ target }) => {
+		if (target.validity.valid) {
+			$('bacon-ipsum').attr({ paras: target.value });
+		}
+	}, { passive: true });
+
+	$('#theme-picker').input(({ target }) => {
+		if (target.value === '') {
 			delete document.documentElement.dataset.theme;
 		} else {
-			document.documentElement.dataset.theme = event.target.value;
+			document.documentElement.dataset.theme = target.value;
 		}
-	}, {passive: true});
+	}, { passive: true });
 });
